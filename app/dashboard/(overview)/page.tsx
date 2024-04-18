51c61cf483977c4a943f174ae252ece7239cbfaf
KevinSu <sumaolin619@gmail.com>
2024-04-18 08:32:01 +0800

    feat(layout): layout | page 配置Metadata信息：title, description, metadatabase信息

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,21 @@
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
 // import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue()
