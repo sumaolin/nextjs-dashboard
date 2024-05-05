021f315558722b94f6a7c255b4afbd2034175cf8
KevinSu <sumaolin619@gmail.com>
2024-05-05 22:35:47 +0800

    style(ui/search): fixed type error

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,7 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((value: string) => {
     const params = new URLSearchParams(searchParams);
-    params.set('page', 1);
+    params.set('page', '1');
     if (value) {
       params.set('query', value);
     } else {
