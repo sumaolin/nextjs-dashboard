3a5a6588668547ce8efc70b8951ffc2fab2eb854
KevinSu <sumaolin619@gmail.com>
2024-05-05 08:07:52 +0800

    feat(invoices/error): 错误展示页面

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -31,10 +31,16 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split('T')[0];
   console.log('time date: ', date);
   console.time();
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    return {
+      message: 'Data Error: Fail to Create Invoice',
+    };
+  }
   console.timeEnd();
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -52,17 +58,35 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    return {
+      message: 'Data Error: Fail to Update Invoice',
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  // 不被 try catch 的 error 才会显示出来 error.tsx 页面
+  throw new Error('Failed to Delete Invoice');
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return {
+      message: 'Delete Success',
+    };
+  } catch (error) {
+    return {
+      message: 'Data Error: fail to Delete Invoice',
+    };
+  }
 }
